4bc98e5033d5e061485df28ba88595fc6f581989
semekatech <georgemuemah@gmail.com>
2024-06-04 10:54:53 +0300

    dfdfdf

--- resources/views/start-trivia.blade.php
+++ resources/views/start-trivia.blade.php
@@ -132,7 +132,7 @@
         <!-- Overlay with countdown timer -->
         <div id="overlay" class="overlaid">
             <div class="countdown">
-                <h2>Trivia starts in <span id="countdown">10</span> seconds</h2>
+                <h2>Trivia starts in <span id="countdown" style="font-size:18px">10</span> seconds</h2>
             </div>
         </div>
 
