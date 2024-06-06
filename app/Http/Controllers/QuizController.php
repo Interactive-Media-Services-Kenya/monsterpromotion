8878433c0a5f391396ce5acd27182ae1d1d3a6c6
semekatech <georgemuemah@gmail.com>
2024-06-06 15:27:00 +0300

    dadada

--- app/Http/Controllers/QuizController.php
+++ app/Http/Controllers/QuizController.php
@@ -12,9 +12,10 @@ use App\Models\QuizAnswer;
 use App\Models\User;
 use Illuminate\Support\Facades\Log;
 use Illuminate\Support\Facades\Storage;
+
 class QuizController extends Controller
 {
-  
+
     public function index()
     {
         return view('backend.dashboard');
@@ -25,7 +26,7 @@ class QuizController extends Controller
     }
     public function viewScore(Request $request)
     {
-        $requestData = $request->all(); 
+        $requestData = $request->all();
         $phone = $requestData['dataToSend']['user_phone'];
         $questionAnswers = json_decode($requestData['dataToSend']['questionAnswers'], true);
 
@@ -34,15 +35,15 @@ class QuizController extends Controller
         foreach ($questionAnswers as $answer) {
             $questionIds[] = $answer['questionId'];
         }
-        $mobile =$phone;
+        $mobile = $phone;
         $numberStr = $mobile;
         if ($numberStr[0] == '0') {
             $mobile2 = "254" . ltrim($mobile, '0');
         } else {
             $mobile2 = $mobile;
         }
-        $user=QuizAnswer::where('user_phone',$mobile2)->first();
-        if($user!="") {
+        $user = QuizAnswer::where('user_phone', $mobile2)->first();
+        if ($user != "") {
             $existingQuestionIds = json_decode($user->question_id, true);
             $updatedQuestionIds = array_values(array_unique(array_merge($existingQuestionIds, $questionIds)));
             $user->question_id = json_encode($updatedQuestionIds);
@@ -52,17 +53,17 @@ class QuizController extends Controller
             // log::debug($questionIdsJson);
             $quizAnswer = new QuizAnswer();
             $quizAnswer->selected_answer = 1;
-            $quizAnswer->question_id =$questionIdsJson;
-            $quizAnswer->category = 1; 
+            $quizAnswer->question_id = $questionIdsJson;
+            $quizAnswer->category = 1;
             $quizAnswer->correct_answer = 1;
-            $quizAnswer->user_phone = $mobile2; 
+            $quizAnswer->user_phone = $mobile2;
             $quizAnswer->save();
             return redirect('user/leaders-board')->with('success', 'Question created successfully.');
-    }
+        }
     }
     public function saveScore(Request $request)
     {
-      
+
         $mobile = (string) $request->phone;
         $mobile2 = $mobile[0] == '0' ? "254" . ltrim($mobile, '0') : $mobile;
         $user = Score::where('phone', $mobile2)->first();
@@ -102,53 +103,58 @@ class QuizController extends Controller
         $question->status = 1;
         $question->correct_answer = $validatedData['correct_answer'];
         $question->save();
-        if($question->save()){
+        if ($question->save()) {
             $answers = [];
             foreach (['A', 'B', 'C', 'D'] as $option) {
                 if ($request->has($option)) {
                     $answers[] = "{$option}:{$request->input($option)}";
                 }
             }
-            $question_answer=new QuestionAnswer;
-            $question_answer->question_id=$question->id;
+            $question_answer = new QuestionAnswer;
+            $question_answer->question_id = $question->id;
             $question_answer->answers = json_encode($answers);
-            $question_answer->status=1;
+            $question_answer->status = 1;
             $question_answer->save();
         }
-   
+
         return redirect('admin/manage-questions')->with('success', 'Question created successfully.');
     }
-    public function manageQuestions(){
-        $questions=Question::where('status',1)->get();
-        return view('backend.quiz/index',['questions'=>$questions]);
+    public function manageQuestions()
+    {
+        $questions = Question::where('status', 1)->get();
+        return view('backend.quiz/index', ['questions' => $questions]);
     }
-    
-    public function playQuiz(){
+
+    public function playQuiz()
+    {
         // $questions=Question::where('status',1)->get();
         return view('start-trivia');
     }
-    public function leadersBoard(){
+    public function leadersBoard()
+    {
         return view('user-results');
     }
-    public function startQuiz(){
+    public function startQuiz()
+    {
         // $questions=Question::where('status',1)->get();
         return view('trivia-questions');
     }
-    
-    public function selectQuiz() {
-        $mobile =$_GET['user_code'];
+
+    public function selectQuiz()
+    {
+        $mobile = $_GET['user_code'];
         $numberStr = $mobile;
         if ($numberStr[0] == '0') {
             $mobile2 = "254" . ltrim($mobile, '0');
         } else {
             $mobile2 = $mobile;
         }
-        $questions_done=QuizAnswer::where('user_phone',$mobile2)->first();
-      if($questions_done){
-        $question_done = json_decode($questions_done->question_id, true) ?: [];
-      }else{
-        $question_done =[];
-      }
+        $questions_done = QuizAnswer::where('user_phone', $mobile2)->first();
+        if ($questions_done) {
+            $question_done = json_decode($questions_done->question_id, true) ?: [];
+        } else {
+            $question_done = [];
+        }
         // dd($question_done);
         if (!isset($_SESSION['random_questions'])) {
             $randomQuestions = Question::whereNotIn('id', $question_done)->get()->shuffle()->take(10);
@@ -171,24 +177,25 @@ class QuizController extends Controller
             return response()->json(['message' => 'Quiz completed']);
         }
     }
-    
-    public function disableQuestions(){
-             $id = decrypt($_GET['id']);
-             $question = Question::find($id);
-             if (!$question) {
-                return redirect()->back()->with('error','Question not found');
-             }
-             try {
-                 $question->update(['status' => 0]);
-                 return redirect()->back()->with('success','Question deleted successfully');
-             } catch (\Exception $e) {
-                return redirect()->back()->with('error','An Error occured');
-             }
+
+    public function disableQuestions()
+    {
+        $id = decrypt($_GET['id']);
+        $question = Question::find($id);
+        if (!$question) {
+            return redirect()->back()->with('error', 'Question not found');
+        }
+        try {
+            $question->update(['status' => 0]);
+            return redirect()->back()->with('success', 'Question deleted successfully');
+        } catch (\Exception $e) {
+            return redirect()->back()->with('error', 'An Error occured');
+        }
     }
     public function sendOTP(Request $request)
     {
-       
-        $mobile =$request["mobile"];
+
+        $mobile = $request["mobile"];
         $otp = rand(100000, 999999);
         $numberStr = $mobile;
         if ($numberStr[0] == '0') {
@@ -196,75 +203,79 @@ class QuizController extends Controller
         } else {
             $mobile2 = $mobile;
         }
-            $user = User::where('phone',$mobile2)->first();
-            if($user->status==1){
-                $exist='approved';
-            }else if($user->status==2){
-                $exist='rejected';
-            }else if($user->status==0){
-                $exist='pending';
+        $exist = 'pending';
+        $user = User::where('phone', $mobile2)->first();
+        if ($user) {
+            if ($user->status == 1) {
+                $exist = 'approved';
+            } else if ($user->status == 2) {
+                $exist = 'rejected';
+            } else {
+                $exist = 'pending';
             }
+        }
+
         try {
             $headers = ["Cookie: ci_session=ttdhpf95lap45hqt3h255af90npbb3ql"];
-          
-                $senderName = rawurlencode("IMS");
-                $bulkBalanceUser = "voucher";
-                $encodMessage = rawurlencode("MONSTER PROMOTIONS\nYour verification code is: $otp.");
-                $url = "https://3.229.54.57/expresssms/Api/send_bulk_api?action=send-sms&api_key=Snh2SGFQT0dIZmFtcRGU9ZXBlcEQ=&to=$mobile2&from=$senderName&sms=$encodMessage&response=json&unicode=0&bulkbalanceuser=$bulkBalanceUser";
-    
-                $ch = curl_init();
-                curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-                curl_setopt($ch, CURLOPT_URL, $url);
-                curl_setopt($ch, CURLOPT_ENCODING, "");
-                curl_setopt($ch, CURLOPT_MAXREDIRS, 10);
-                curl_setopt($ch, CURLOPT_TIMEOUT, 0);
-                curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, false);
-                curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
-                curl_setopt($ch, CURLOPT_FOLLOWLOCATION, true);
-                curl_setopt($ch, CURLOPT_HTTP_VERSION, CURL_HTTP_VERSION_1_1);
-                curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");
-                $response = curl_exec($ch);
-                $res = json_decode($response);
-                curl_close($ch);
-                return response()->json(["status" => "success","exist"=>$exist,"code"=>$otp, "message" => "OTP requested successfully"]);
+
+            $senderName = rawurlencode("IMS");
+            $bulkBalanceUser = "voucher";
+            $encodMessage = rawurlencode("MONSTER PROMOTIONS\nYour verification code is: $otp.");
+            $url = "https://3.229.54.57/expresssms/Api/send_bulk_api?action=send-sms&api_key=Snh2SGFQT0dIZmFtcRGU9ZXBlcEQ=&to=$mobile2&from=$senderName&sms=$encodMessage&response=json&unicode=0&bulkbalanceuser=$bulkBalanceUser";
+
+            $ch = curl_init();
+            curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+            curl_setopt($ch, CURLOPT_URL, $url);
+            curl_setopt($ch, CURLOPT_ENCODING, "");
+            curl_setopt($ch, CURLOPT_MAXREDIRS, 10);
+            curl_setopt($ch, CURLOPT_TIMEOUT, 0);
+            curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, false);
+            curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+            curl_setopt($ch, CURLOPT_FOLLOWLOCATION, true);
+            curl_setopt($ch, CURLOPT_HTTP_VERSION, CURL_HTTP_VERSION_1_1);
+            curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");
+            $response = curl_exec($ch);
+            $res = json_decode($response);
+            curl_close($ch);
+            return response()->json(["status" => "success", "exist" => $exist, "code" => $otp, "message" => "OTP requested successfully"]);
         } catch (\Exception $e) {
             Log::debug($e);
             return response()->json(["status" => "error", "message" => "Unable to request OTP."]);
         }
     }
-    
+
     public function saveSelfie(Request $request)
     {
-        $mobile =$request->input('phone');
+        $mobile = $request->input('phone');
         $numberStr = $mobile;
         if ($numberStr[0] == '0') {
             $mobile2 = "254" . ltrim($mobile, '0');
         } else {
             $mobile2 = $mobile;
         }
-        $user=User::where('phone',$mobile2)->first();
-       
-                if($user){
-                    if($user->status==1){
-                        return response()->json(["status" => "approved"]);
-                    }else{
-                        return response()->json(["status" => "pending"]);  
-                    }
-                    
-                }else{
-                    $file = $request->file('file');
-                    $directory = 'public/user-uploads';
-                    if (!Storage::exists($directory)) {
-                        Storage::makeDirectory($directory);
-                    }
-                    $filePath = $file->store($directory);
-                    $user = new User;
-                    $user->phone =$mobile2;
-                    $user->status =0;
-                    $user->photo = $filePath;
-                    $user->save();
-                    return response()->json(["status" => "pending"]);  
-                }
-       
+        $user = User::where('phone', $mobile2)->first();
+
+        if ($user) {
+            if ($user->status == 1) {
+                return response()->json(["status" => "approved"]);
+            } else {
+                return response()->json(["status" => "pending"]);
+            }
+
+        } else {
+            $file = $request->file('file');
+            $directory = 'public/user-uploads';
+            if (!Storage::exists($directory)) {
+                Storage::makeDirectory($directory);
+            }
+            $filePath = $file->store($directory);
+            $user = new User;
+            $user->phone = $mobile2;
+            $user->status = 0;
+            $user->photo = $filePath;
+            $user->save();
+            return response()->json(["status" => "pending"]);
+        }
+
     }
 }
--- public/js/indexpage.js
+++ public/js/indexpage.js
@@ -2,93 +2,92 @@
 
 const formput = document.querySelector(".upload-form");
 const form = document.querySelector("form"),
-fileInput = document.querySelector(".file-input");
+  fileInput = document.querySelector(".file-input");
 phoneInput = document.getElementById("phone_no").value;
 
 verifyButton = document.querySelector(".verify-button"),
-verificationCodeInput = document.getElementById("verificationid"),
-progressArea = document.querySelector(".progress-area"),
-uploadedArea = document.querySelector(".uploaded-area");
+  verificationCodeInput = document.getElementById("verificationid"),
+  progressArea = document.querySelector(".progress-area"),
+  uploadedArea = document.querySelector(".uploaded-area");
 fileInput.disabled = true;
 verificationCodeInput.disabled = true;
-document.getElementById("submitbtn").disabled=true;
-formput.addEventListener("click", () =>{
+document.getElementById("submitbtn").disabled = true;
+formput.addEventListener("click", () => {
 
-fileInput.click();
+  fileInput.click();
 });
 verifyButton.addEventListener("click", () => {
-var phone = document.getElementById("phone_no").value;
+  var phone = document.getElementById("phone_no").value;
 
-if (validateMobileNumber(phone)) {
-$.ajax({
-url: '/api/user/send-otp',
-method: 'POST',
-data: {
-mobile: phone,
-_token: '{{ csrf_token() }}'
-},
-success: function (response) {
-  console.log(response);
-if (response.status === 'success') {
-if(response.exist=='approved'){
-  localStorage.setItem('user_mobile_no',phone);
-document.querySelector(".upload-form").style.display = 'none';
-}else if(response.exist=='rejected'){
-  document.querySelector(".response-message").innerText='Your previous upload was rejected,Kindly upload again';
-}else{
-  document.querySelector(".upload-form").style.display = 'none';
-  document.querySelector(".response-message").innerText='Your previous upload is still pending,Please try again later.';
-}
-      var verification_otp=response.code;
-      localStorage.setItem('verification_otp',verification_otp);
-    toastr.success('OTP requested successfully!');
-    verificationCodeInput.disabled = false;
-} else {
-    // toastr.error(response.message);
-    Swal.fire({
-        icon: 'error',
-        title: 'Error',
-        text: 'OTP Not Send.'
-    }).then((result) => {
-        if (result.isConfirmed) {
-            window.location.href = '/';
+  if (validateMobileNumber(phone)) {
+    $.ajax({
+      url: '/api/user/send-otp',
+      method: 'POST',
+      data: {
+        mobile: phone,
+        _token: '{{ csrf_token() }}'
+      },
+      success: function (response) {
+        console.log(response);
+        if (response.status === 'success') {
+          if (response.exist == 'approved') {
+            localStorage.setItem('user_mobile_no', phone);
+            document.querySelector(".upload-form").style.display = 'none';
+          } else if (response.exist == 'rejected') {
+            document.querySelector(".response-message").innerText = 'Your previous upload was rejected,Kindly upload again';
+          } else {
+            document.querySelector(".upload-form").style.display = 'none';
+            document.querySelector(".response-message").innerText = 'Your previous upload is still pending,Please try again later.';
+          }
+          var verification_otp = response.code;
+          localStorage.setItem('verification_otp', verification_otp);
+          toastr.success('OTP requested successfully!');
+          verificationCodeInput.disabled = false;
+        } else {
+          Swal.fire({
+            icon: 'error',
+            title: 'Error',
+            text: 'OTP Not Send.'
+          }).then((result) => {
+            if (result.isConfirmed) {
+              window.location.href = '/';
+            }
+          });
         }
+      },
+      error: function (xhr, status, error) {
+        toastr.error('Error: Unable to request OTP.');
+      }
     });
-}
-},
-error: function (xhr, status, error) {
-toastr.error('Error: Unable to request OTP.');
-}
+  } else {
+    Swal.fire({
+      icon: 'error',
+      title: 'Error',
+      text: 'Please Enter a valid mobile No'
+    });
+  }
 });
-} else {
-Swal.fire({
-icon: 'error',
-title: 'Error',
-text: 'Please Enter a valid mobile No'
-});
-}
-});
-fileInput.onchange = ({target})=>{
-let file = target.files[0];
-if(file){
-let fileName = file.name;
-if(fileName.length >= 12){
-let splitName = fileName.split('.');
-fileName = splitName[0].substring(0, 13) + "... ." + splitName[1];
-}
-uploadFile(fileName);
-}
+fileInput.onchange = ({ target }) => {
+  let file = target.files[0];
+  if (file) {
+    let fileName = file.name;
+    if (fileName.length >= 12) {
+      let splitName = fileName.split('.');
+      fileName = splitName[0].substring(0, 13) + "... ." + splitName[1];
+    }
+    uploadFile(fileName);
+  }
 }
 
-function uploadFile(name){
-let xhr = new XMLHttpRequest();
-xhr.open("POST", "api/user/uspload-photo");
-xhr.upload.addEventListener("progress", ({loaded, total}) =>{
-let fileLoaded = Math.floor((loaded / total) * 100);
-let fileTotal = Math.floor(total / 1000);
-let fileSize;
-(fileTotal < 1024) ? fileSize = fileTotal + " KB" : fileSize = (loaded / (1024*1024)).toFixed(2) + " MB";
-let progressHTML = `<li class="row">
+function uploadFile(name) {
+  let xhr = new XMLHttpRequest();
+  xhr.open("POST", "api/user/uspload-photo");
+  xhr.upload.addEventListener("progress", ({ loaded, total }) => {
+    let fileLoaded = Math.floor((loaded / total) * 100);
+    let fileTotal = Math.floor(total / 1000);
+    let fileSize;
+    (fileTotal < 1024) ? fileSize = fileTotal + " KB" : fileSize = (loaded / (1024 * 1024)).toFixed(2) + " MB";
+    let progressHTML = `<li class="row">
           <i class="fas fa-file-alt"></i>
           <div class="content">
             <div class="details">
@@ -100,11 +99,11 @@ let progressHTML = `<li class="row">
             </div>
           </div>
         </li>`;
-uploadedArea.classList.add("onprogress");
-progressArea.innerHTML = progressHTML;
-if(loaded == total){
-progressArea.innerHTML = "";
-let uploadedHTML = `<li class="row">
+    uploadedArea.classList.add("onprogress");
+    progressArea.innerHTML = progressHTML;
+    if (loaded == total) {
+      progressArea.innerHTML = "";
+      let uploadedHTML = `<li class="row">
             <div class="content upload">
               <i class="fas fa-file-alt"></i>
               <div class="details">
@@ -114,50 +113,51 @@ let uploadedHTML = `<li class="row">
             </div>
             <i class="fas fa-check"></i>
           </li>`;
-uploadedArea.classList.remove("onprogress");
-uploadedArea.insertAdjacentHTML("afterbegin", uploadedHTML);
-}
-});
-let data = new FormData(form);
-xhr.send(data);
+      uploadedArea.classList.remove("onprogress");
+      uploadedArea.insertAdjacentHTML("afterbegin", uploadedHTML);
+    }
+  });
+  let data = new FormData(form);
+  xhr.send(data);
 }
 function validateMobileNumber(number) {
-console.log(number);
-number = number.replace(/\D/g, '');
-const length = number.length;
-if ((number.startsWith('07') || number.startsWith('01')) && length === 10) {
-return true; 
-} else if ((number.startsWith('254') || number.startsWith('254')) && length ===12 ) {
-return true;
-} else {
-return false;
-} }
+  console.log(number);
+  number = number.replace(/\D/g, '');
+  const length = number.length;
+  if ((number.startsWith('07') || number.startsWith('01')) && length === 10) {
+    return true;
+  } else if ((number.startsWith('254') || number.startsWith('254')) && length === 12) {
+    return true;
+  } else {
+    return false;
+  }
+}
 
 var ellipsis = document.getElementById("ellipsis");
 var intervalId;
 function startAnimation() {
-var ellipsis = document.getElementById("ellipsis");
-var count = 1;
-var maxCount = 1;
-var increasing = true; 
+  var ellipsis = document.getElementById("ellipsis");
+  var count = 1;
+  var maxCount = 1;
+  var increasing = true;
 
-intervalId = setInterval(function() {
-if (increasing) {
-// Increase dot count
-ellipsis.innerHTML += ".";
-count++;
-if (count > maxCount) {
-increasing = false;
-}
-} else {
-// Decrease dot count
-ellipsis.innerHTML = ellipsis.innerHTML.slice(0, -1);
-count--;
-if (count === 1) {
-increasing = true; // Start increasing dot count again
-}
-}
-}, 200); // Adjust the interval duration as needed
+  intervalId = setInterval(function () {
+    if (increasing) {
+      // Increase dot count
+      ellipsis.innerHTML += ".";
+      count++;
+      if (count > maxCount) {
+        increasing = false;
+      }
+    } else {
+      // Decrease dot count
+      ellipsis.innerHTML = ellipsis.innerHTML.slice(0, -1);
+      count--;
+      if (count === 1) {
+        increasing = true; // Start increasing dot count again
+      }
+    }
+  }, 200); // Adjust the interval duration as needed
 }
 
 // Start the animation
@@ -166,112 +166,114 @@ startAnimation();
 
 // Function to stop the animation
 function stopAnimation() {
-clearInterval(intervalId);
-ellipsis.innerHTML = ""; // Remove ellipsis when animation stops
+  clearInterval(intervalId);
+  ellipsis.innerHTML = ""; // Remove ellipsis when animation stops
 }
 
 function verifyOTP() {
-var verificationCodeInput = document.getElementById("verificationid").value;
-if (verificationCodeInput != "") {
-document.getElementById("verifying_code").textContent = "Verifying code"; 
-document.getElementById("verifying_code").style.display = 'inline-block';
-document.getElementById("ellipsis").style.display = 'inline-block';
-var otp_code= document.getElementById("verificationid").value;
-var otp_retrieved=localStorage.getItem('verification_otp');
-;
-startAnimation();
-if(otp_code.length==6){
-if(otp_retrieved==otp_code){
-fileInput.disabled = false;
-document.getElementById("verifying_code").textContent = "Verified Succesfully"; 
-document.getElementById("verifying_code").style.color = '#56be78';
-document.getElementById("ellipsis").style.display = 'none';
+  var verificationCodeInput = document.getElementById("verificationid").value;
+  if (verificationCodeInput != "") {
+    document.getElementById("verifying_code").textContent = "Verifying code";
+    document.getElementById("verifying_code").style.display = 'inline-block';
+    document.getElementById("ellipsis").style.display = 'inline-block';
+    var otp_code = document.getElementById("verificationid").value;
+    var otp_retrieved = localStorage.getItem('verification_otp');
+    ;
+    startAnimation();
+    if (otp_code.length == 6) {
+      if (otp_retrieved == otp_code) {
+        fileInput.disabled = false;
+        document.getElementById("verifying_code").textContent = "Verified Succesfully";
+        document.getElementById("verifying_code").style.color = '#56be78';
+        document.getElementById("ellipsis").style.display = 'none';
 
-document.getElementById("submitbtn").disabled=false;
+        document.getElementById("submitbtn").disabled = false;
 
-}else{
-document.getElementById("verifying_code").textContent = "Wrong Verification Code";  
-document.getElementById("ellipsis").style.display = 'none'; 
+      } else {
+        document.getElementById("verifying_code").textContent = "Wrong Verification Code";
+        document.getElementById("ellipsis").style.display = 'none';
+      }
+    }
+  }
 }
-}}}
 
 function saveDetails() {
-let file = fileInput.files[0];
-let phone = document.getElementById("phone_no").value.trim();
-if (document.querySelector(".upload-form").style.display === 'none') {
+  let file = fileInput.files[0];
+  let phone = document.getElementById("phone_no").value.trim();
+  if (document.querySelector(".upload-form").style.display === 'none') {
 
-document.getElementById("submitbtn").disabled=false;
-}else{
-if (!file) {
-Swal.fire({
-icon: 'error',
-title: 'Error',
-text: 'Please select a file to upload.'
-});
+    document.getElementById("submitbtn").disabled = false;
+  } else {
+    if (!file) {
+      Swal.fire({
+        icon: 'error',
+        title: 'Error',
+        text: 'Please select a file to upload.'
+      });
 
-}
-}
+    }
+  }
 
 
-if (!phone) {
-Swal.fire({
-icon: 'error',
-title: 'Error',
-text: 'Please enter a phone number.'
-});
-return;
-}
-let data = new FormData(form);
-console.log(data);
-$.ajax({
-url: 'api/user/save-user-details',
-method: 'POST',
-data: data,
-processData: false,
-contentType: false, 
-headers: {
-'X-CSRF-TOKEN': '{{ csrf_token() }}' 
-},
-success: function(response) {
-  console.log(response.status);
-if(response.status=='approved'){
-  Swal.fire({
-    icon: 'success',
-    title: 'Success',
-    text: 'Details submitted, you will be redirected shortly.'
+  if (!phone) {
+    Swal.fire({
+      icon: 'error',
+      title: 'Error',
+      text: 'Please enter a phone number.'
     });
-    setTimeout(function() {
-      window.location.href = 'user/play-games';
-      }, 1000);
-      localStorage.setItem('user_mobile_no',phone);
-}else{
-  Swal.fire({
-    icon: 'info',
-    title: 'Pending Approval',
-    text: 'Your uploaded selfie is pending approval. We shall get back to you shortly.',
-    showCancelButton: true,
-    confirmButtonText: 'OK',
-    // cancelButtonText: 'Cancel'
-  }).then((result) => {
-    if (result.isConfirmed) {
-      // Reload the page
-      location.reload();
+    return;
+  }
+  let data = new FormData(form);
+  console.log(data);
+  $.ajax({
+    url: 'api/user/save-user-details',
+    method: 'POST',
+    data: data,
+    processData: false,
+    contentType: false,
+    headers: {
+      'X-CSRF-TOKEN': '{{ csrf_token() }}'
+    },
+    success: function (response) {
+      console.log(response.status);
+      if (response.status == 'approved') {
+        Swal.fire({
+          icon: 'success',
+          title: 'Success',
+          text: 'Details submitted, you will be redirected shortly.'
+        });
+        setTimeout(function () {
+          window.location.href = 'user/play-games';
+        }, 1000);
+        localStorage.setItem('user_mobile_no', phone);
+      } else {
+        Swal.fire({
+          icon: 'info',
+          title: 'Pending Approval',
+          text: 'Your uploaded selfie is pending approval. We shall get back to you shortly.',
+          showCancelButton: true,
+          confirmButtonText: 'OK',
+          // cancelButtonText: 'Cancel'
+        }).then((result) => {
+          if (result.isConfirmed) {
+            // Reload the page
+            location.reload();
+          }
+        });
+
+
+      }
+
+    },
+    error: function (xhr, status, error) {
+      if (response.status == 'failed_phone') {
+        Swal.fire({
+          icon: 'error',
+          title: 'error',
+          text: 'Phone Cannot be empty.'
+        });
+
+      }
     }
   });
-  
-
-}
-
-},
-error: function(xhr, status, error) {
-if(response.status=='failed_phone'){
-Swal.fire({
-icon: 'error',
-title: 'error',
-text: 'Phone Cannot be empty.'
-});
-
-}
-}
-});
 }
