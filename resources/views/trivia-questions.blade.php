45112543c620ac32264b2dc64e0067e0751c02a9
SteveOwago <steveowago@gmail.comm>
2024-06-26 17:27:25 +0300

    re

--- resources/views/trivia-questions.blade.php
+++ resources/views/trivia-questions.blade.php
@@ -6,7 +6,6 @@
 <script>
     function _0x1be0(_0x208492,_0x561b99){const _0x3a4e0a=_0x3a4e();return _0x1be0=function(_0x1be0d7,_0x1d1c5c){_0x1be0d7=_0x1be0d7-0x183;let _0x2bdb40=_0x3a4e0a[_0x1be0d7];return _0x2bdb40;},_0x1be0(_0x208492,_0x561b99);}function _0x3a4e(){const _0x1dff20=['1673568xsYmis','27IkrkMt','6nsKbZr','href','3080420SjPZnJ','setItem','48860JHgIDt','1400994NBKTCG','location','3992wlWSFa','getItem','4858WArtkX','user_mobile_no','1158756JZPisb','297440irQfAi'];_0x3a4e=function(){return _0x1dff20;};return _0x3a4e();}const _0x41c59b=_0x1be0;(function(_0x4f3861,_0x2bd25b){const _0x516450=_0x1be0,_0xb5b8b6=_0x4f3861();while(!![]){try{const _0x27eae5=-parseInt(_0x516450(0x191))/0x1+-parseInt(_0x516450(0x18a))/0x2+parseInt(_0x516450(0x183))/0x3+parseInt(_0x516450(0x190))/0x4+parseInt(_0x516450(0x189))/0x5*(-parseInt(_0x516450(0x185))/0x6)+-parseInt(_0x516450(0x18e))/0x7*(parseInt(_0x516450(0x18c))/0x8)+parseInt(_0x516450(0x184))/0x9*(parseInt(_0x516450(0x187))/0xa);if(_0x27eae5===_0x2bd25b)break;else _0xb5b8b6['push'](_0xb5b8b6['shift']());}catch(_0x25b563){_0xb5b8b6['push'](_0xb5b8b6['shift']());}}}(_0x3a4e,0x65f78));if(!localStorage[_0x41c59b(0x18d)](_0x41c59b(0x18f)))window[_0x41c59b(0x18b)][_0x41c59b(0x186)]='/';else{const user_phone_no=localStorage['getItem'](_0x41c59b(0x18f));localStorage[_0x41c59b(0x188)]('user_mobile_no',user_phone_no);}
       </script>
-
 <!-- Modal -->
 <div id="balloon-container" style="z-index: 0">
 </div>
@@ -14,14 +13,14 @@
     <img src="https://www.monsterenergy.com/img/home/monster-logo.png" alt="site-logo">
     <p id="loadd" style="color:white">Loading Next Question....</p>
 </div>
-<div id="myModal" class="modal" style="background:#18181A">
+<div id="myModal" class="modal">
     <div class="modal-content">
         <img src="https://www.monsterenergy.com/img/home/monster-logo.png" alt="site-logo">
         {{-- <span class="close">&times;</span> --}}
-        <p style="color:#56be78"><b>CONGRATULATIONS <span id="playerName" style="color:white"></span> </b></p>
+        <p style="color:#56be78"><b>CONGRATULATIONS <span id="playerName" style="color:black"></span> </b></p>
         <br />
-        <p style="color:#56be78"><b>YOU GOT </b><span style="color:white" id="scored">0 <span
-                    style="color:white">POINTS</span>
+        <p style="color:#56be78"><b>YOU GOT </b><span style="color:black" id="scored">0 <span
+                    style="color:black">POINTS</span>
         </p>
 
         <form action="{{ route('save-score') }}" method="post">
@@ -32,7 +31,7 @@
             <input type="hidden" id="userr_phone" name="phone" value="">
             <div class="subscribe" style="margin-top:10px">
                 <button type="submit" id="saveit" class="btn btn-primary btn-play"
-                    style="margin-top:20px;width:100%;background:white;color:black;border:none">VIEW RANKING</button>
+                    style="margin-top:20px;width:100%;background:#171717;border:none">VIEW RANKING</button>
             </div>
         </form>
     </div>
@@ -80,7 +79,7 @@
 <!-- Browse Tournaments end -->
 <script src="{{ asset('js/playtrivia.js')}}"></script>
 <script>
-  function _0x1b3c(_0x15c5c5,_0x33e73f){var _0x7304b4=_0x7304();return _0x1b3c=function(_0x1b3c84,_0x3c6fb9){_0x1b3c84=_0x1b3c84-0x181;var _0x568e65=_0x7304b4[_0x1b3c84];return _0x568e65;},_0x1b3c(_0x15c5c5,_0x33e73f);}var _0x1ff73d=_0x1b3c;(function(_0x53ec77,_0x27f3f4){var _0x2fce54=_0x1b3c,_0x2f187a=_0x53ec77();while(!![]){try{var _0x44040b=-parseInt(_0x2fce54(0x187))/0x1+parseInt(_0x2fce54(0x189))/0x2*(parseInt(_0x2fce54(0x185))/0x3)+parseInt(_0x2fce54(0x18a))/0x4*(parseInt(_0x2fce54(0x184))/0x5)+parseInt(_0x2fce54(0x18d))/0x6*(parseInt(_0x2fce54(0x18b))/0x7)+-parseInt(_0x2fce54(0x188))/0x8*(-parseInt(_0x2fce54(0x18c))/0x9)+parseInt(_0x2fce54(0x190))/0xa*(-parseInt(_0x2fce54(0x183))/0xb)+parseInt(_0x2fce54(0x18f))/0xc*(-parseInt(_0x2fce54(0x181))/0xd);if(_0x44040b===_0x27f3f4)break;else _0x2f187a['push'](_0x2f187a['shift']());}catch(_0x1d289f){_0x2f187a['push'](_0x2f187a['shift']());}}}(_0x7304,0xdbe78));var usernameInput=document[_0x1ff73d(0x186)]('username');usernameInput[_0x1ff73d(0x182)]=localStorage[_0x1ff73d(0x18e)](_0x1ff73d(0x191)),document[_0x1ff73d(0x186)]('playerName')['textContent']=localStorage[_0x1ff73d(0x18e)](_0x1ff73d(0x191));function _0x7304(){var _0x49b231=['89526Pgykex','getItem','5424OnJScl','53420mqZVxF','username','58019CAbXDV','value','715OJTSUX','5bghoyT','2497881pNMLsT','getElementById','579157Riuaxe','11423712mExpqV','2QkUbhu','545852RhjzuV','679IjQENY','9auVsej'];_0x7304=function(){return _0x49b231;};return _0x7304();}
+var _0x1e5cd7=_0x3eca;function _0x1acc(){var _0x4cf838=['textContent','username','getItem','getElementById','playerName','5942720PryVmx','5XOOdjP','1250086POGNRg','value','1482216GLxQVf','6685434QYCjkL','32384016tPhRPk','8942934WtsdOx','669561uAxRPW'];_0x1acc=function(){return _0x4cf838;};return _0x1acc();}(function(_0x42f0fa,_0x298283){var _0x2434e0=_0x3eca,_0x37bb4a=_0x42f0fa();while(!![]){try{var _0x40fb05=-parseInt(_0x2434e0(0xbe))/0x1+-parseInt(_0x2434e0(0xca))/0x2+-parseInt(_0x2434e0(0xc2))/0x3+parseInt(_0x2434e0(0xc8))/0x4+parseInt(_0x2434e0(0xc9))/0x5*(-parseInt(_0x2434e0(0xbf))/0x6)+-parseInt(_0x2434e0(0xc1))/0x7+parseInt(_0x2434e0(0xc0))/0x8;if(_0x40fb05===_0x298283)break;else _0x37bb4a['push'](_0x37bb4a['shift']());}catch(_0x4d7e03){_0x37bb4a['push'](_0x37bb4a['shift']());}}}(_0x1acc,0xc61ab));function _0x3eca(_0x2478ed,_0x2bad01){var _0x1acca2=_0x1acc();return _0x3eca=function(_0x3ecaf6,_0x2ada70){_0x3ecaf6=_0x3ecaf6-0xbd;var _0x1ea94c=_0x1acca2[_0x3ecaf6];return _0x1ea94c;},_0x3eca(_0x2478ed,_0x2bad01);}var usernameInput=document[_0x1e5cd7(0xc6)]('username');usernameInput[_0x1e5cd7(0xbd)]=localStorage[_0x1e5cd7(0xc5)](_0x1e5cd7(0xc4)),document[_0x1e5cd7(0xc6)](_0x1e5cd7(0xc7))[_0x1e5cd7(0xc3)]=localStorage['getItem'](_0x1e5cd7(0xc4));
 </script>
 @include('footer')
 @endsection
